6ba393abbc4f2879ea3a91b8581abba20a24b378
Bruno Americo Camoleze <bruno.camoleze@MB-BRUNO.local>
2017-09-27 15:35:59 -0300

    Mapeamento de entidades pkg (Titulo, Entidade)

---
+++ financeiro/src/main/java/br/com/crud/financeiro/model/Entidade.java
@@ -0,0 +1,104 @@
+package br.com.crud.financeiro.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tab_entidades")
+public class Entidade {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@Column(length = 60)
+	private String nome;
+	
+	@Column(length = 20)
+	private String cpfCnpj;
+	
+	@Column(length = 100)
+	private String endereco;
+	
+	@Column(length = 30)
+	private String cidade;
+	
+	@Column(length = 30)
+	private String bairro;
+	
+	@Column(length = 2)
+	private String estado;
+	
+	@Column(length = 20)
+	private String telefone;
+	
+	@Column(length = 100)
+	private String email;
+	
+	@OneToMany(mappedBy = "entidade")
+	private List<Titulo> titulos = new ArrayList<>();
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ financeiro/src/main/java/br/com/crud/financeiro/model/Titulo.java
@@ -0,0 +1,91 @@
+package br.com.crud.financeiro.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="tab_titulos")
+public class Titulo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	//Já carrega as entidades do titulo
+	@ManyToOne(fetch = FetchType.LAZY) 
+	@JoinColumn(name="entidadeID")
+	private Entidade entidade;
+	
+	
+	private Date dataDeEmissao;
+	private Date dataDeValidade; 
+	private Date dataDoPagamento;
+	private BigDecimal valor;
+	private BigDecimal valorPago;
+	private BigDecimal valorOriginal;
+	
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public Entidade getEntidade() {
+		return entidade;
+	}
+	public void setEntidade(Entidade entidade) {
+		this.entidade = entidade;
+	}
+	public Date getDataDeEmissao() {
+		return dataDeEmissao;
+	}
+	public void setDataDeEmissao(Date dataDeEmissao) {
+		this.dataDeEmissao = dataDeEmissao;
+	}
+	public Date getDataDeValidade() {
+		return dataDeValidade;
+	}
+	public void setDataDeValidade(Date dataDeValidade) {
+		this.dataDeValidade = dataDeValidade;
+	}
+	public Date getDataDoPagamento() {
+		return dataDoPagamento;
+	}
+	public void setDataDoPagamento(Date dataDoPagamento) {
+		this.dataDoPagamento = dataDoPagamento;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public BigDecimal getValorPago() {
+		return valorPago;
+	}
+	public void setValorPago(BigDecimal valorPago) {
+		this.valorPago = valorPago;
+	}
+	public BigDecimal getValorOriginal() {
+		return valorOriginal;
+	}
+	public void setValorOriginal(BigDecimal valorOriginal) {
+		this.valorOriginal = valorOriginal;
+	}
+	
+	
+
+}
+
+
